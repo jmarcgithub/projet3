daaf0975b3fbf7efd86faeb7bb2512db66f7fbae
Jean Marc <jmpiano61@gmail.com>
2019-10-13 07:39:59 +0200

    premier programme console labyrinthe

---
+++ lab.py
@@ -0,0 +1,19 @@
+#-*- coding: utf8 -*-
+def look(lab01, lab02, lab03, lab04, lab05, lab06, lab07, lab08, lab09, lab10, lab11, lab12, lab13, lab14, lab15, compteur):
+	print(lab01)
+	print(lab02)
+	print(lab03)
+	print(lab04)
+	print(lab05)
+	print(lab06)
+	print(lab07)
+	print(lab08)
+	print(lab09)
+	print(lab10)
+	print(lab11)
+	print(lab12)
+	print(lab13)
+	print(lab14)
+	print(lab15)
+	print("nombre de cadeaux pris = ", compteur)
+	
